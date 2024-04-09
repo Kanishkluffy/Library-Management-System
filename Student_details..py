a52fbe07a79483b1517990f4471b829b38408180
Kanishk <ktadhithya2006@gmail.com>
2024-04-09 21:28:57 +0530

    First Commit

---
+++ Student_details..py
@@ -0,0 +1,207 @@
+import pandas as pd
+
+print("---------------------LIBRARY MANAGEMENT SYSTEM-----------------------")
+
+#Reading the data from the CSV File
+df1=pd.read_csv('student_details.csv')
+df2=pd.read_csv('book_details.csv')
+
+def addmember():
+    global df1
+    #Getting inputs from the user
+    print("ENTER YOUR DETAILS TO GET ADDED IN OUR LIBRARY")
+    name=input("Enter your name:")
+    name=name.lower()
+    sapid=int(input("Enter your Sap ID:"))
+    if len(str(sapid)) != 9: 
+        print("Enter your correct sapid")
+        exit()
+    password=input("Enter your Password:")
+
+    if(sapid not in df1['Sap ID'].values):
+        #Storing the user inputed values in a Dictionary
+        dict2={'Student Name':name,'Sap ID':sapid,'Password':password}
+
+        #Creating a DataFrame for the given dictionary
+        df=pd.DataFrame(dict2, index=[0])
+
+        #Adding the user inputed values to the CSV file by using concat method
+        df1=pd.concat([df1,df],ignore_index=True)
+
+        #Writing the added values to the CSV file
+        df1.to_csv("student_details.csv", index=False)
+    else:
+        print("Name already exist")
+
+def displaymember():
+    global df1
+    #Displaying the Student details
+    print(df1[['Student Name','Sap ID']])
+
+def deletemember():
+    global df1
+    print("ENTER THE NAME & SAP WHICH YOU WANT TO DELETE")
+    name=input("Enter the name:")
+    name = name.lower()
+    sapid=int(input("Enter the sapid:"))
+
+    #Input name and sapid from the user and check whether the Name exist in the data
+    if(name in df1['Student Name'].values and sapid in df1['Sap ID'].values):
+        #Getting the index of the name entered by the user
+        x = df1.loc[df1['Student Name'] == name].index
+
+        #Deleting the record from the data
+        df1 = df1.drop(x, axis=0)
+        df1.to_csv("student_details.csv", index=False)
+        print(df1)
+    else:
+        print("Your Student Name or Sap ID is wrong")
+
+def modifymember():
+    global df1
+    print("ENTER THE DETAILS TO MODIFY THE DETAILS OF STUDENT IN OUR LIBRARY")
+    name=input("Enter the Name to be modified:")
+    name=name.lower()
+    pas=input("Enter the Password to be modified:")
+    if(name in df1['Student Name'].values):
+        x = df1.loc[df1['Student Name'] == name].index
+        df1.loc[x, "Student Name"] = name
+        df1.loc[x, "Password"] = pas
+    df1.to_csv("student_details.csv", index=False)
+
+def addbook():
+    global df2
+    print("ENTER THE DETAILS TO ADD THE BOOK IN OUR LIBRARY")
+    bookcod=int(input("Enter the Book Code:"))
+    booknam=input("Enter the Book Name:")
+    authnam=input("Enter the Author Name:")
+    genre=input("Enter the Genre:")
+    avail=int(input("Enter the Total number of books:"))
+    if(bookcod not in df2['Book Code'].values):
+        dict1={'Book Code':bookcod,'Book Name':booknam,'Author Name':authnam,'Genre':genre,'Available':avail}
+        df=pd.DataFrame(dict1, index=[0])
+        df2=pd.concat([df2,df],ignore_index=True)
+        df2.to_csv('book_details.csv',index=False)
+    else:
+        print("Book already exist")
+
+def modifybook():
+    global df2
+    print("ENTER THE DETAILS TO MODIFY THE BOOK IN OUR LIBRARY")
+
+    ch=int(input("Enter which details to be modified\n1.Book Name\n2.Author Name\n3.Available\n4.Genre\n"))
+    bookcod=int(input("Enter the Book Code to be modified:"))
+    x = df2.loc[df2['Book Code'] == bookcod].index
+    if(bookcod in df2['Book Code'].values):
+        if ch==1:
+            booknam=input("Enter the Book Name to be modified:")
+            df2.loc[x, "Book Name"] = booknam
+        elif ch==2:
+            authname=input("Enter the Author Name to be modified:")
+            df2.loc[x, "Author Name"] = authname
+        elif ch==3:
+            avail=int(input("Enter the Available books to be modified:"))
+            df2.loc[x, "Available"] = avail
+        elif ch==4:
+            genre=input("Enter the genre to be modified:")
+            df2.loc[x, "Genre"] = genre
+    df2.to_csv('book_details.csv',index=False)
+
+def displaybook():
+    print(df2)
+
+def deletebook():
+    global df2
+    print("ENTER THE BOOK CODE AND BOOK NAME WHICH YOU WANT TO DELETE")
+    bookcod=int(input("Enter the Book Code:"))
+    if(bookcod in df2['Book Code'].values):
+        x = df2.loc[df2['Book Code'] == bookcod].index
+        df2 = df2.drop(x, axis=0)
+        df2.to_csv('book_details.csv',index=False)
+    else:
+        print("Entered Book Code doesn't exist")
+
+print("ENTER THE LOGIN DETAILS")
+dup_name="Kanishk"
+dup_pas="Kanishk123"
+name=input("Enter the Name:")
+pas=input("Enter the Password:")
+
+if(name == dup_name and pas == dup_pas):
+    print("WELCOME!")
+
+def main_program():
+    print("\nWHICH OPERATION YOU NEED TO PERFORM")
+    ch=int(input("Enter the choice\n1.Add Member\n2.Display Member\n3.Delete Member\n4.Modify Member\n5.Add Book\n6.Modify Book\n7.Display Book\n8.Delete Book\n"))
+    if ch==1:
+        n=int(input("Enter how many members do you need to add:"))
+        for i in range(n):
+            addmember()
+        m=input("Do you want to continue(y/n):")
+        if m=='y':
+            main_program()
+        else:
+            exit()
+    elif ch==2:
+        displaymember()
+        m=input("Do you want to continue(y/n):")
+        if m=='y':
+            main_program()
+        else:
+            exit()
+    elif ch==3:
+        n=int(input("Enter how many members do you need to delete:"))
+        for i in range(n):
+            deletemember()
+        m=input("Do you want to continue(y/n):")
+        if m=='y':
+            main_program()
+        else:
+            exit()
+    elif ch==4:
+        n=int(input("Enter how many members do you need to modify:"))
+        for i in range(n):
+            modifymember()
+        m=input("Do you want to continue(y/n):")
+        if m=='y':
+            main_program()
+        else:
+            exit()
+    elif ch==5:
+        n=int(input("Enter how many books do you need to add:"))
+        for i in range(n):
+            addbook()
+        m=input("Do you want to continue(y/n):")
+        if m=='y':
+            main_program()
+        else:
+            exit()
+    elif ch==6:
+        n=int(input("Enter how many books do you need to modify:"))
+        for i in range(n):
+            modifybook()
+        m=input("Do you want to continue(y/n):")
+        if m=='y':
+            main_program()
+        else:
+            exit()
+    elif ch==7:
+        displaybook()
+        m=input("Do you want to continue(y/n):")
+        if m=='y':
+            main_program()
+        else:
+            exit()
+    elif ch==8:
+        n=int(input("Enter how many books do you need to delete:"))
+        for i in range(n):
+            deletebook()
+        m=input("Do you want to continue(y/n):")
+        if m=='y':
+            main_program()
+        else:
+            exit()
+    else:
+        print("Enter the valid choice")
+
+main_program()
---
+++ borrow_details.py
@@ -0,0 +1,76 @@
+import pandas as pd 
+import random
+
+df1=pd.read_csv('student_details.csv')
+df5=pd.read_csv("Borrow_Details.csv")
+df3=pd.read_csv("book_details.csv")
+
+def borrowbook():
+    global df5
+    L=[]
+    booknam=input("Enter your book name:")
+    bookcod=int(input("Enter the book code:"))
+    copi=int(input("Enter the number of Copies you want:"))
+    g = df3.loc[df3['Book Code'] == bookcod].index
+    if(bookcod in df3['Book Code'][g].values and df3['Available'][g].values[0] >= copi):
+        while(copi):
+            j=random.randint(100,999)
+            bookid=f"DDN{bookcod}.{j}"
+            df3.loc[g,'Available'] = df3['Available'][g]-1
+            f = df5.loc[df5['Book ID'] == bookid].index
+            status=df5.loc[f,'Status'] = "Borrowed"
+            d={'Student Name':name,'Sap ID':sapid,'Book Name':df3['Book Name'][g].values[0],'Author Name':df3['Author Name'][g].values[0],
+               'Genre':df3['Genre'][g].values[0],'Book Code':bookcod,'Book ID':bookid,'Status':status}
+            L.append(d)
+            copi -= 1
+        print(df3)
+        df5 = pd.DataFrame(L, columns=['Student Name','Sap ID','Book Name','Author Name','Genre','Book Code','Book ID','Status'])
+        df5.to_csv('Borrow_Details.csv', index=False)
+        df3.to_csv('book_details.csv',index=False)
+    else:
+        print("Entered book code or Copies doesn't exist")
+
+def returnbook():
+    global df5
+    booknam=input("Enter your book name:")
+    bookcod=int(input("Enter the Book Code:"))
+    copi=int(input("Enter the number of Copies you need to return:"))
+    count=len(df5[(df5['Book Code']==bookcod) & (df5['Student Name']==name)])
+    if(copi<=count):
+        for i in range(copi):
+            f = df3.loc[df3['Book Code'] == bookcod].index
+            bookid=input("Enter your book ID:")
+            a = df5.loc[df5['Book ID'] == bookid].index
+            df5.loc[a,'Status'] = "Returned"
+            df3.loc[f,'Available'] = df3['Available'][f]+1
+            print(df3)
+    else:
+        print("Enter the valid copies")
+        exit()
+    df5.to_csv('Borrow_Details.csv', index=False)
+    df3.to_csv('book_details.csv', index=False)
+
+def displaybook():
+    print(df3)
+
+
+print("ENTER THE LOGIN DETAILS")
+name=input("Enter your Name:")
+name=name.lower()
+sapid=int(input("Enter your Sap ID:"))
+pas=input("Enter your Password:")
+b = df1.loc[df1['Sap ID'] == sapid].index
+f = df1.loc[df1['Password'] == pas].index
+c = df1.loc[df1['Student Name'] == name].index
+if(sapid in df1['Sap ID'][b].values and pas in df1['Password'][f].values and name in df1['Student Name'][c].values):
+    ch=int(input((("ENTER THE OPERATION NEED TO PERFORM\n1.Borrow Book\n2.Return Book\n3.Display Book\n"))))
+    if ch==1:
+        borrowbook()
+    elif ch==2:
+        returnbook()
+    elif ch==3:
+        displaybook()
+    else:
+        print('Enter the valid choice')
+else:
+    print("Entered Student details doesn't exist")
